d4abecfd283d4e328dba87f4770785bfba19d5b9
Hiromasa Yasuda <hyasuda.phys@gmail.com>
2021-07-25 17:10:38 +0900

    minor update

--- projects/rectangles/src/main.rs
+++ projects/rectangles/src/main.rs
@@ -5,10 +5,6 @@ struct Rectangle {
 }
 
 impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
